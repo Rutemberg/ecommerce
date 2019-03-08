b64b8843cd907ba7e4f2254bdbb4bbc48c3cbee8
Rutemberg <rutembergbarbosa@gmail.com>
2019-03-08 14:26:48 -0300

    Recuperar Senha

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -2,12 +2,14 @@
 
 namespace Hcode\Model;
 use Hcode\DB\Sql;
+use Hcode\Mailer;
 use Hcode\Model;
 
 
 class User extends Model
 {
     const SESSION = "user";
+
 
     public static function login($login, $password){
 
@@ -70,14 +72,6 @@ class User extends Model
     public function save()
     {
 
-        /*
-        pdesperson VARCHAR(64),
-        pdeslogin VARCHAR(64),
-        pdespassword VARCHAR(256),
-        pdesemail VARCHAR(128),
-        pnrphone BIGINT,
-        pinadmin TINYINT
-        */
         $sql = new Sql();
         $results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
             ":desperson"=>$this->getdesperson(),
@@ -125,6 +119,109 @@ class User extends Model
             ":iduser"=>$this->getiduser()
         ));
     }
+
+    public static function getForgot($email){
+
+        define("SECRET_IV", pack("a16", 'senha'));
+        define("SECRET", pack("a16", 'senha'));
+
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_persons a INNER JOIN tb_users b USING(Idperson) WHERE a.desemail = :EMAIL", array(
+            ":EMAIL"=>$email
+        ));
+
+        if (count($results) === 0) {
+
+            throw new \Exception("Não foi possivel recuperar a senha.");
+
+        }else
+        {
+
+            $data = $results[0];
+
+            $results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+                ":iduser"=>$data['iduser'],
+                ":desip"=>$_SERVER['REMOTE_ADDR']
+            ));
+
+            if (count($results2) === 0) {
+
+                throw new \Exception("Não foi possivel recuperar a senha.");
+
+            }else
+            {
+                $dataRecovery = $results2[0];
+
+                $code= openssl_encrypt(
+                    json_encode($dataRecovery["idrecovery"]),
+                    'AES-128-CBC',
+                    SECRET,
+                    0,
+                    SECRET_IV
+                );
+
+                $link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+
+                $mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir senha do Rutemberg store", "forgot", array(
+                    "name"=>$data["desperson"],
+                    "link"=>$link
+                ));
+
+                $mailer->send();
+
+                return $data;
+
+            }
+        }
+
+    }
+
+    public static function validForgotDecrypt($code){
+        define("SECRET_IV", pack("a16", 'senha'));
+        define("SECRET", pack("a16", 'senha'));
+
+        $idRecovery = json_decode(openssl_decrypt($code, 'AES-128-CBC', SECRET, 0, SECRET_IV));
+
+
+        $sql = new Sql();
+        $results = $sql->select("SELECT *
+             FROM tb_userspasswordsrecoveries a
+             INNER JOIN tb_users b USING(iduser)
+             INNER JOIN tb_persons c USING(idperson)
+             WHERE a.idrecovery = :idrecovery AND a.dtrecovery
+             IS NULL AND DATE_ADD(a.dtregister, INTERVAL 2 HOUR) >= NOW();", array(
+                 ":idrecovery"=>$idRecovery
+             ));
+
+        if (count($results) === 0) {
+            throw new \Exception("Não foi possivel recuperar a senha.");
+
+        }
+        else
+        {
+            return $results[0];
+        }
+    }
+
+
+    public static function setForgotUsed($idrecovery)
+    {
+        $sql = new Sql();
+        $sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+            ":idrecovery"=>$idrecovery
+        ));
+
+    }
+
+    public function setPassword($password)
+    {
+        $sql = new Sql();
+        $sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
+            ":password"=>$password,
+            ":iduser"=>$this->getiduser()
+        ));
+    }
+
 }
 
  ?>
